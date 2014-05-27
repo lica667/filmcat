637e5725260253e527d0ea8f5f7b738d6dc6a920
Tanya <lica667@gmail.com>
2014-05-27 12:14:28 +0300

    initial filmcat

---
+++ filmcat/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Filmcat::Application.config.secret_key_base = '759c26316cc826190cc12f18bbf6b07dd4b4a39b6eaf71d4958e0dc543c03131c49266ecbf8ee976be0e13fe625fd1c7945c271095b677e22d1158222bcc4735'
